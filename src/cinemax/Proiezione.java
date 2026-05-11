6b15b8bd0253609a1656eb2a187565303ad73c64
Fabio <fabio.proserpio.06@gmail.com>
2026-05-11 10:20:33 +0200

    Controlli inserimento film

--- src/cinemax/Proiezione.java
+++ src/cinemax/Proiezione.java
@@ -64,4 +64,72 @@ public class Proiezione {
             e.printStackTrace();
         }
     }
+
+    public static String inserisciTitolo(Scanner input){
+        String titolo;
+
+        do {
+            System.out.println("Inserisci il titolo del film: ");
+            titolo = input.nextLine().trim();
+        } while (titolo.isEmpty());
+
+        return titolo;
+    }
+
+    public static String inserisciGenere(Scanner input){
+        String genere;
+
+        do {
+            System.out.println("Inserisci il genere del film: ");
+            genere = input.nextLine().trim();
+        } while (genere.isEmpty());
+
+        return genere;
+    }
+
+    public static String inserisciRegista(Scanner input){
+        String regista;
+
+        do {
+            System.out.println("Inserisci il regista del film: ");
+            regista = input.nextLine().trim();
+        } while (regista.isEmpty());
+
+        return regista;
+    }
+
+    public static int inserisciAnno(Scanner input){
+        int anno;
+
+        do {
+            System.out.println("Inserisci l'anno del film ");
+            anno = input.nextInt();
+        } while (anno<=1950 || anno>=2026);
+
+        return anno;
+    }
+
+    public static int  inserisciDurata(Scanner input){
+        int durata;
+
+        do {
+            System.out.println("Inserisci la durata del film ");
+            durata = input.nextInt();
+        } while (durata<0 ||durata >300);
+
+        return durata;
+    }
+
+    public static int inserisciEtaMinima(Scanner input){
+        int eta;
+
+        do {
+            System.out.println("Inserisci l'eta minima per visionare il film ");
+            eta = input.nextInt();
+        } while (eta<0 || eta>19);
+
+        return eta;
+    }
+
+
 }
