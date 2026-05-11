1a108b8efced74438e75b9df4d2f8171879b844e
diegouninsubria <diegouninsubria@gmail.com>
2026-05-11 15:46:28 +0200

    aggiunto menu a  Cinemax.java

--- src/cinemax/Cinemax.java
+++ src/cinemax/Cinemax.java
@@ -1,16 +1,48 @@
 package cinemax;
 
 public class Cinemax {
-    public static void main(String args[]){
-        Guest g=new Guest();
-        //g.Registrazione();
-        Utente ut=null;
-        ut=g.Login();
-        if(ut==null){
-            System.out.println("utente non presente");
-        }
-        else
-             System.out.println("ti sei loggato come:"+ut.toString());
+    public static void main(String args[]) {
 
+        Menu menu = new Menu();
+        Guest g = new Guest();
+        Utente ut = null;
+
+        int scelta = menu.mostraMenuIniziale();
+
+        switch(scelta){
+
+            case 1: 
+                ut = g.Login();
+                if(ut == null){
+                    System.out.println("Utente non presente");
+                } else {
+                    System.out.println("Ti sei loggato come:");
+                    System.out.println(ut.toString());
+                }
+                break;
+
+            case 2: 
+                g.Registrazione();
+                break;
+
+            case 3: 
+                System.out.println("Accesso come Guest...");
+                int sceltaGuest = menu.mostraMenuGuest();
+
+                switch(sceltaGuest){
+                    case 1:
+                        ut = g.Login();
+                        break;
+
+                    case 2:
+                        g.Registrazione();
+                        break;
+
+                    case 3:
+                        System.out.println("Funzione ricerca proiezioni (da implementare)");
+                        break;
+                }
+                break;
+        }
     }
 }
