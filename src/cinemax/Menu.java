a688cb2d1dda96501cee1d3e4ade0330ad23aecc
Fabio <fabio.proserpio.06@gmail.com>
2026-05-20 17:57:27 +0200

    Correzione menu cerca Proiezione

--- src/cinemax/Menu.java
+++ src/cinemax/Menu.java
@@ -158,22 +158,17 @@ public class Menu {
         do{
             System.out.println("===MENU CERCA PRENOTAZIONE===");
             System.out.println("1) Ricerca per la data della Proiezione");
-            System.out.println("2) Ricerca per l'ora della Proiezione");
-            System.out.println("3) Ricerca per il titolo del film");
-            System.out.println("4) Ricerca per genere del film");
-            System.out.println("5) Ricerca per il regista del film ");
-            System.out.println("6) Ricerca per l'anno di uscita del film");
-            System.out.println("7) Ricerca per la durata del film");
-            System.out.println("8) Ricerca per l'età minima del film");
-            System.out.println("9) Ricerca per costo della proeizione");
-            System.out.println("10) Ricerca combinata");
+            System.out.println("2) Ricerca per il titolo del film");
+            System.out.println("3) Ricerca per genere del film");
+            System.out.println("4) Ricerca per costo della proeizione");
+            System.out.println("5) Ricerca combinata");
 
             System.out.println("========================");
             System.out.print("Scelta: ");
 
             scelta = input.nextInt();
             input.nextLine();
-        }while(scelta<=0 ||scelta>=11);
+        }while(scelta<=0 ||scelta>=6);
 
         return scelta;
     }
