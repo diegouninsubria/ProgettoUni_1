28dd001a1ef316489dd6a730875f11114e1d24b1
Fabio <fabio.proserpio.06@gmail.com>
2026-05-22 15:41:38 +0200

    aggiunta \n

--- src/cinemax/Menu.java
+++ src/cinemax/Menu.java
@@ -8,32 +8,30 @@ import java.util.Scanner;
  * (Guest, Cliente, Proiezionista, Bigliettaio) e restituisce la scelta
  * selezionata tramite input da tastiera
  */
-
 public class Menu {
 
     /**
      * Mostra il menu iniziale dell'applicazione, permettendo all'utente
-     * di scegliere tra login, registrazioneaccesso come Guest o uscita.
+     * di scegliere tra login, registrazione accesso come Guest o uscita.
      *
      * @return scelta selezionata dall'utente (valore compreso tra 1 e 4)
      */
-
     public int mostraMenuIniziale() {
         Scanner input = new Scanner(System.in);
         int scelta;
 
         do {
-            System.out.println("===== MENU CINEMAX =====");
-            System.out.println("1) Login");
-            System.out.println("2) Registrazione");
-            System.out.println("3) Accedi come Guest");
-            System.out.println("4) Esci");
-            System.out.println("========================");
+            System.out.println("===== MENU CINEMAX =====\n");
+            System.out.println("1) Login\n");
+            System.out.println("2) Registrazione\n");
+            System.out.println("3) Accedi come Guest\n");
+            System.out.println("4) Esci\n");
+            System.out.println("========================\n");
             System.out.print("Scelta: ");
 
             scelta = input.nextInt();
             input.nextLine();
-        }while(scelta<=0 || scelta>=5);
+        } while (scelta <= 0 || scelta >= 5);
 
         return scelta;
     }
@@ -44,13 +42,11 @@ public class Menu {
      *
      * @return scelta selezionata dall'utente (valore compreso tra 1 e 3)
      */
-
-    public int mostraMenuGuest(){
+    public int mostraMenuGuest() {
         Scanner input = new Scanner(System.in);
         int scelta;
 
         do {
-
             System.out.println("===== MENU GUEST =====\n");
             System.out.println("1) Login\n");
             System.out.println("2) Registrazione\n");
@@ -60,7 +56,8 @@ public class Menu {
 
             scelta = input.nextInt();
             input.nextLine();
-        }while(scelta<=0||scelta>=4);
+        } while (scelta <= 0 || scelta >= 4);
+
         return scelta;
     }
 
@@ -70,25 +67,24 @@ public class Menu {
      *
      * @return scelta selezionata dall'utente (valore compreso tra 1 e 6)
      */
-
-    public int mostraMenuCliente(){
+    public int mostraMenuCliente() {
         Scanner input = new Scanner(System.in);
         int scelta;
 
         do {
-            System.out.println("===== MENU CLIENTE=====\n");
-            System.out.println("1) Visualliza le tue prenotazioni\n");
+            System.out.println("===== MENU CLIENTE =====\n");
+            System.out.println("1) Visualizza le tue prenotazioni\n");
             System.out.println("2) Aggiungi prenotazione\n");
             System.out.println("3) Modifica proiezione\n");
             System.out.println("4) Elimina proiezione\n");
-            System.out.println("5) visualizza proiezioni\n");
+            System.out.println("5) Visualizza proiezioni\n");
             System.out.println("6) Logout\n");
             System.out.println("========================\n");
             System.out.print("Scelta: ");
 
             scelta = input.nextInt();
             input.nextLine();
-        }while(scelta<=0 || scelta>=7);
+        } while (scelta <= 0 || scelta >= 7);
 
         return scelta;
     }
@@ -98,37 +94,37 @@ public class Menu {
      *
      * @return scelta selezionata dall'utente (valore compreso tra 1 e 5)
      */
-
-    public int mostraMenuProiezionista(){
+    public int mostraMenuProiezionista() {
         Scanner input = new Scanner(System.in);
         int scelta;
 
         do {
-            System.out.println("===== MENU CI =====\n");
+            System.out.println("===== MENU PROIEZIONISTA =====\n");
             System.out.println("1) Aggiungi film\n");
             System.out.println("2) Aggiungi proiezione\n");
-            System.out.println("3) Modifica proiezione\n\n");
-            System.out.println("4) Elimina proiezione");
+            System.out.println("3) Modifica proiezione\n");
+            System.out.println("4) Elimina proiezione\n");
             System.out.println("5) Logout\n");
             System.out.println("========================\n");
             System.out.print("Scelta: ");
 
             scelta = input.nextInt();
             input.nextLine();
-        } while(scelta<=0 || scelta>=6);
+        } while (scelta <= 0 || scelta >= 6);
+
         return scelta;
     }
 
     /**
-     * Mostra il menu edicato al Bigliettaio, che può cercare prenotazioni
+     * Mostra il menu dedicato al Bigliettaio, che può cercare prenotazioni
      * o effettuare il logout.
      *
      * @return scelta selezionata dall'utente (valore compreso tra 1 e 2)
      */
-
-    public int mostraMenuBigliettaio(){
+    public int mostraMenuBigliettaio() {
         Scanner input = new Scanner(System.in);
         int scelta;
+
         do {
             System.out.println("===== MENU BIGLIETTAIO =====\n");
             System.out.println("1) Cerca prenotazione\n");
@@ -138,85 +134,82 @@ public class Menu {
 
             scelta = input.nextInt();
             input.nextLine();
-        }while(scelta<=0 || scelta>=3);
+        } while (scelta <= 0 || scelta >= 3);
 
         return scelta;
     }
 
     /**
      * Mostra il menu dedicato alla ricerca delle proiezioni, permettendo
-     * all'utente di selezionare il criterio di ricerca desiderato
-     * (data, ora, titolo, genere, regista, anno, durata, età minima,
-     * costo oppure ricerca combinata).
+     * all'utente di selezionare il criterio di ricerca desiderato.
      *
-     * @return scelta selezionata dall'utente (valore compreso tra 1 e 10)
+     * @return scelta selezionata dall'utente (valore compreso tra 1 e 5)
      */
-
-    public int mostraMenuCercaProeizione(){
+    public int mostraMenuCercaProeizione() {
         Scanner input = new Scanner(System.in);
         int scelta;
-        do{
-            System.out.println("===MENU CERCA PRENOTAZIONE===");
-            System.out.println("1) Ricerca per la data della Proiezione");
-            System.out.println("2) Ricerca per il titolo del film");
-            System.out.println("3) Ricerca per genere del film");
-            System.out.println("4) Ricerca per costo della proeizione");
-            System.out.println("5) Ricerca combinata");
 
-            System.out.println("========================");
+        do {
+            System.out.println("=== MENU CERCA PROIEZIONE ===\n");
+            System.out.println("1) Ricerca per la data della Proiezione\n");
+            System.out.println("2) Ricerca per il titolo del film\n");
+            System.out.println("3) Ricerca per genere del film\n");
+            System.out.println("4) Ricerca per costo della proiezione\n");
+            System.out.println("5) Ricerca combinata\n");
+            System.out.println("========================\n");
             System.out.print("Scelta: ");
 
             scelta = input.nextInt();
             input.nextLine();
-        }while(scelta<=0 ||scelta>=6);
+        } while (scelta <= 0 || scelta >= 6);
 
         return scelta;
     }
 
-    public int sceltaBigliettaioRicerca(){
+    public int sceltaBigliettaioRicerca() {
         Scanner input = new Scanner(System.in);
         int scelta;
 
-        do{
-            System.out.println("===MENU CERCA PRENOTAZIONE===");
-            System.out.println("1) Ricerca per ID");
-            System.out.println("2) Ricerca nome e cognome");
-            System.out.println("3) Ricerca per titolo del film");
-            System.out.println("4) Ricerca intervallo data");
-            System.out.println("========================");
+        do {
+            System.out.println("=== MENU CERCA PRENOTAZIONE ===\n");
+            System.out.println("1) Ricerca per ID\n");
+            System.out.println("2) Ricerca nome e cognome\n");
+            System.out.println("3) Ricerca per titolo del film\n");
+            System.out.println("4) Ricerca intervallo data\n");
+            System.out.println("========================\n");
             System.out.print("Scelta: ");
 
             scelta = input.nextInt();
             input.nextLine();
-        }while(scelta<1 || scelta>4);
+        } while (scelta < 1 || scelta > 4);
 
         return scelta;
     }
-    public int MenuSceltaGenere(){
+
+    public int MenuSceltaGenere() {
         Scanner input = new Scanner(System.in);
         int scelta;
 
-        do{
-            System.out.println("===MENU SCELTA GENERE===");
-            System.out.println("1) ACTION");
-            System.out.println("2) ADVENTURE");
-            System.out.println("3) ANIMATION");
-            System.out.println("4) BIOGRAPHY");
-            System.out.println("5) COMEDY");
-            System.out.println("6) CRIME");
-            System.out.println("7) DRAMA");
-            System.out.println("8) FILM-NOIR");
-            System.out.println("9) HORROR");
-            System.out.println("10) MISTERY");
-            System.out.println("11) WESTERN");
-            System.out.println("========================");
+        do {
+            System.out.println("=== MENU SCELTA GENERE ===\n");
+            System.out.println("1) ACTION\n");
+            System.out.println("2) ADVENTURE\n");
+            System.out.println("3) ANIMATION\n");
+            System.out.println("4) BIOGRAPHY\n");
+            System.out.println("5) COMEDY\n");
+            System.out.println("6) CRIME\n");
+            System.out.println("7) DRAMA\n");
+            System.out.println("8) FILM-NOIR\n");
+            System.out.println("9) HORROR\n");
+            System.out.println("10) MYSTERY\n");
+            System.out.println("11) WESTERN\n");
+            System.out.println("========================\n");
             System.out.print("Scelta: ");
 
             scelta = input.nextInt();
             input.nextLine();
-        }while(scelta<1 || scelta>11);
+        } while (scelta < 1 || scelta > 11);
 
         return scelta;
     }
-
 }
